ea64d224a1b46ef6565792e4579cd51bfe84beb4
kasunsampath <adskasunsampath@gmail.com>
2018-10-09 21:00:27 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,127 +1,165 @@
-
+var mysql = require('mysql');
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
-const jsonWebToken = require('jsonwebtoken');
-const mysql = require('mysql');
-
-app.use(bodyParser.json()); // only parses json data
-app.use(bodyParser.urlencoded({ // handles the urlencoded bodies
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
     extended: true
 }));
+ 
+ 
 
-const myJWTSecretKey = 'my-secret-key'; // set secret key, which we will use for creating and decoding JWT tokens, keep it safe.
 
+
+
+
+ 
 // connection configurations
 const mc = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
     password: '',
-    database: 'node_api_new'
+    database: 'RESTAPI'
 });
-
+ 
 // connect to database
 mc.connect();
+ 
 
 
-app.post('/salespersonLogin', (req, res) => {
-
-    let username = mc.escape(req.body.username);
-    let password = mc.escape(req.body.password);
-
-    if (!username) {
-        return res.status(400).send({ error: true, message: 'Please provide username' });
-    }
-
-    if (!password) {
-        return res.status(400).send({ error: true, message: 'Please provide password' });
-    }
 
 
-    mc.query('SELECT salesperson_code,salesperson_code_count FROM sales_person where salesperson_username=? AND salesperson_password=?', [username, password], function (error, results, fields) {
+
+// default route
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'hello' })
+});
+ 
+// port must be set to 8080 because incoming http requests are routed from port 80 to port 8080
+app.listen(8080, function () {
+    console.log('Node app is running on port 8080');
+});
+
+
+
+// Retrieve all todos 
+app.get('/todos', function (req, res) {
+    mc.query('SELECT * FROM tasks', function (error, results, fields) {
         if (error) throw error;
-        if (results.length == 0) {
-            return res.send({ error: true, message: 'Invalid username or password' });
-        } else if (results.length == 1) {
-            const user = {
-                un: username,
-                pw: password
-            };
+        return res.send({ error: false, data: results, message: 'Todos list.',num:results.length });
+    });
+});
 
 
-            // sign with default (HMAC SHA256) 
-            const token = jsonWebToken.sign(user, myJWTSecretKey);
-            res.json({
-                error: false,
-                token: token,
-                message: 'Successfully logged in',
-                data: results
-            });
 
-        } else {
-            return res.send({ error: true, message: 'More users' });
-        }
-        //return res.send({ error: false, data: results[0], message: results.length });
+
+// Retrieve todo with id 
+app.get('/todo/:id', function (req, res) {
+ 
+    let task_id = req.params.id;
+ 
+    if (!task_id) {
+        return res.status(400).send({ error: true, message: 'Please provide task_id' });
+    }
+
+    mc.query('SELECT * FROM tasks where id=?', task_id, function (error, results, fields) {
+        if(results.length>0){
+if (error) throw error;
+        return res.send({ error: false, data: results[0], message: 'Todos list.' });
+
+}
+else{
+if (error) throw error;
+        return res.send({ error: true,  message: 'id not valid' });
+}
+    });
+ 
+});
+
+
+
+
+
+// Search for todos with ‘bug’ in their name
+app.get('/todos/search/:keyword', function (req, res) {
+    let keyword = req.params.keyword;
+
+    mc.query("SELECT * FROM tasks WHERE task LIKE ? ", ['%' + keyword + '%'], function (error, results, fields) {
+  if(results.length>0){
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Todos search list.' });
+}
+else{
+if (error) throw error;
+        return res.send({ error: true,  message: 'keyword not found' });
+}
     });
 
 
 
-
 });
 
-function isValidToken(token) {
-    try {
-        const tokenDecodedData = jsonWebToken.verify(token, myJWTSecretKey);
-        return true;
-    } catch (error) {
-        return false;
+
+
+
+// Add a new todo  
+app.post('/todo', function (req, res) {
+ 
+    let task = req.body.task;
+ 
+    if (!task) {
+        return res.status(400).send({ error:true, message: 'Please provide task' });
     }
-}
-
-// GET - http://localhost:3000/verify/{token}
-app.get('/verify/:token', (req, res) => {
-    try {
-        const tokenDecodedData = jsonWebToken.verify(req.params.token, myJWTSecretKey);
-        return res.json({
-            error: false,
-            data: tokenDecodedData
-        });
-    } catch (error) {
-        res.json({
-            error: true,
-            data: error
-        });
-    }
-})
-
-
-// Retrieve todo with id 
-app.get('/products', function (req, res) {
-    let token = mc.escape(req.headers.authorization);
-
-    if (!token) {
-        return res.status(400).send({ error: true, message: 'Please provide token' });
-    }
-
-    if (isValidToken(token)) {
-        mc.query('SELECT a.name as product_name,sum(b.order_items_qty) as product_quantity FROM product as a,sales_person_items as b where a.idproduct=b.product_idproduct  Group By a.name', function (error, results, fields) {
-            if (error) throw error;
-            if (results.length > 0)
-                return res.send({ error: false, data: results, message: 'Products list' });
-            else
-                return res.send({ error: true, data: results, message: 'Product list is empty' });
-        });
-    } else {
-        res.json({
-            error: true,
-            data: "Invalid Token"
-        });
-    }
-
-
-
+ 
+    mc.query("INSERT INTO tasks SET ? ", { task: task }, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'New task has been created successfully.' });
+    });
 });
 
-app.listen(3000, () => {
-    console.log(`Server is running at: 8080`);
+
+
+//  Delete todo
+app.delete('/todo/:id', function (req, res) {
+ 
+    //let task_id = req.body.task_id;
+ let task_id = req.params.id
+ 
+    if (!task_id) {
+        return res.status(400).send({ error: true, message: 'Please provide task_id' });
+    }
+    mc.query('DELETE FROM tasks WHERE id = ?', [task_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
+    });
+});
+
+
+
+
+//  Update todo with id
+app.put('/todo', function (req, res) {
+ 
+    let task_id = req.body.task_id;
+    let task = req.body.task;
+ 
+    if (!task_id || !task) {
+        return res.status(400).send({ error: task, message: 'Please provide task and task_id' });
+    }
+ 
+    mc.query("UPDATE tasks SET task = ? WHERE id = ?", [task, task_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
+    });
+});
+
+
+
+
+// all other requests redirect to 404
+app.all("*", function (req, res, next) {
+    return res.send({ error: true,  message: 'Page not found' });
+    next();
 });
